1f1a5e1bab62961ba248127a9344fb50aac63431
Xiaofeng Han <xiaofeng.han@oracle.com>
2025-09-21 10:43:29 -0700

    Command Factory

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/CommandFactory.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.command;
+
+import com.arcrobotics.ftclib.command.Command;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.AutonDriveTrain;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.TeleopDrivetrain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CommandFactory {
+
+    final Telemetry telemetry;
+    final AutonDriveTrain autonDriveTrain;
+    final TeleopDrivetrain teleopDrivetrain;
+    final Intake intake;
+    final Shooter shooter;
+
+    public Command driveToTarget(DriveToTargetCommand.DriveParameters driveParameters) {
+        return new DriveToTargetCommand(autonDriveTrain, telemetry, driveParameters);
+    }
+
+    public Command intake() {
+        throw new NotImplementedException();
+    }
+
+    public void shoot() {
+        throw new NotImplementedException();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.command.CommandFactory;
 import org.firstinspires.ftc.teamcode.opmodes.OpModeTemplate;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Shooter;
@@ -15,6 +16,8 @@ public class MainTeleop extends OpModeTemplate {
 
     Shooter shooter;
 
+    CommandFactory commandFactory;
+
     @Override
     public void initialize() {
         super.initialize();
@@ -22,6 +25,8 @@ public class MainTeleop extends OpModeTemplate {
         this.drive = new TeleopDrivetrain(hardwareMap, driverGamepad, telemetry);
         this.intake = new Intake(hardwareMap, operatorGamepad, telemetry);
         this.shooter = new Shooter(hardwareMap, operatorGamepad, telemetry);
+
+        this.commandFactory = new CommandFactory(telemetry, null, drive, intake, shooter);
 
         register(drive, intake, shooter);
     }
