67f21890452db42f5c0aaa0acd4f948ee752ba67
TonyHan2 <tony.han.lele@gmail.com>
2025-10-19 10:16:06 -0700

    imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/AutonDriveTrainCalibrate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/AutonDriveTrainCalibrate.java
@@ -5,7 +5,6 @@ import android.util.Log;
 import com.arcrobotics.ftclib.command.RunCommand;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.opmodes.OpModeTemplate;
