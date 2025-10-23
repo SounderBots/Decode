52fe3efaf430bcb212ea483ab3d12681e38fa609
Aarush Abraham <aarush.abraham@gmail.com>
2025-10-22 18:00:20 -0700

    Test double barrel shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.opmodes.OpModeTemplate;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
-import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.DoubleShooter;
 import org.firstinspires.ftc.teamcode.subsystems.TeleopDrivetrain;
 
 @TeleOp
@@ -13,7 +15,7 @@ public class MainTeleop extends OpModeTemplate {
     TeleopDrivetrain drive;
     Intake intake;
 
-    Shooter shooter;
+    DoubleShooter doubleShooter;
 
     @Override
     public void initialize() {
@@ -21,8 +23,20 @@ public class MainTeleop extends OpModeTemplate {
 
         this.drive = new TeleopDrivetrain(hardwareMap, driverGamepad, telemetry);
         this.intake = new Intake(hardwareMap, operatorGamepad, telemetry);
-        this.shooter = new Shooter(hardwareMap, operatorGamepad, telemetry);
+        this.doubleShooter = new DoubleShooter(hardwareMap, operatorGamepad, telemetry);
 
-        register(drive, intake, shooter);
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.Y)
+                .whenPressed(new InstantCommand(doubleShooter::TurnShooterOn, doubleShooter));
+
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.A)
+                .whenPressed(new InstantCommand(doubleShooter::RightBallLaunch, doubleShooter));
+
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.X)
+                .whenPressed(new InstantCommand(doubleShooter::RightBallReset, doubleShooter));
+
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.B)
+                .whenPressed(new InstantCommand(doubleShooter::RightBallStow, doubleShooter));
+
+        register(drive, intake, doubleShooter);
     }
 }
