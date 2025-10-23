06147014ee83c47a16e5e78df87bd2ea9a4ff4d3
Aarush Abraham <aarush.abraham@gmail.com>
2025-10-22 18:24:48 -0700

    Single Shooter teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SingleShooterTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.opmodes.OpModeTemplate;
+import org.firstinspires.ftc.teamcode.subsystems.DoubleShooter;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.SingleShooter;
+import org.firstinspires.ftc.teamcode.subsystems.TeleopDrivetrain;
+
+@TeleOp
+public class SingleShooterTest extends OpModeTemplate {
+
+    TeleopDrivetrain drive;
+    Intake intake;
+
+    SingleShooter singleShooter;
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        this.drive = new TeleopDrivetrain(hardwareMap, driverGamepad, telemetry);
+        this.intake = new Intake(hardwareMap, operatorGamepad, telemetry);
+        this.singleShooter = new SingleShooter(hardwareMap, operatorGamepad, telemetry);
+
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.Y)
+                .whenPressed(new InstantCommand(singleShooter::TurnShooterOn, singleShooter));
+
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.A)
+                .whenPressed(new InstantCommand(singleShooter::BallLaunch, singleShooter));
+
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.X)
+                .whenPressed(new InstantCommand(singleShooter::BallReset, singleShooter));
+
+        operatorGamepad.getGamepadButton(GamepadKeys.Button.B)
+                .whenPressed(new InstantCommand(singleShooter::BallStow, singleShooter));
+
+        register(drive, intake, singleShooter);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SingleShooter.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class SingleShooter extends SubsystemBase {
+
+    Telemetry telemetry;
+    GamepadEx gamepad;
+    Motor leftFlywheel, rightFlywheel;
+
+    Servo leftLauncher, rightLauncher;
+
+    public SingleShooter(HardwareMap hardwareMap, GamepadEx gamepad, Telemetry telemetry) {
+        this.gamepad = gamepad;
+        this.telemetry = telemetry;
+
+        this.rightFlywheel = new Motor(hardwareMap, "RightFlywheel");
+
+        this.rightLauncher = hardwareMap.get(Servo.class,"RightLauncher");
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+
+        double leftYValue = gamepad.getLeftY();
+
+        if(Math.abs(leftYValue) > .1) {
+            rightFlywheel.set(leftYValue);
+            leftFlywheel.set(-1 * leftYValue);
+        }
+    }
+
+    public void TurnShooterOn() {
+        rightFlywheel.set(1);
+        leftFlywheel.set(-1);
+    }
+
+    public void TurnShooterOff() {
+        rightFlywheel.set(0);
+        leftFlywheel.set(0);
+    }
+
+    public void BallLaunch() {
+        rightLauncher.setPosition(1);
+    }
+
+    public void BallStow() {
+        rightLauncher.setPosition(.45);
+    }
+
+    public void BallReset() {
+        rightLauncher.setPosition(0);
+    }
+}
