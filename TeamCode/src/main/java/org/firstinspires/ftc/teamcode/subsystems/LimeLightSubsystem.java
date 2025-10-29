3dfb7ad3ce9d4673c76fc474248945c7ba4fb4d0
AaronW6489 <aaron.w8656@gmail.com>
2025-10-28 18:38:31 -0700

    Final AprilTag distance code using Limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLightSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLightSubsystem.java
@@ -6,6 +6,7 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
@@ -18,10 +19,6 @@ public class LimeLightSubsystem extends SubsystemBase {
     protected Telemetry telemetry;
 
     private static final int PIPELINE_ID = 4; // April tag pipeline id
-
-    private static final double CAMERA_HEIGHT = 0.5; // meters
-    private static final double TARGET_HEIGHT = 1.0; // meters (center of AprilTag)
-    private static final double CAMERA_MOUNT_ANGLE = 15.0; // degrees
 
     //Needs to be removed
     @Override
@@ -56,11 +53,9 @@ public class LimeLightSubsystem extends SubsystemBase {
         double distance = 0d;
         if (fr != null) {
             int tagID = fr.getFiducialId();
-//            double xDeg = fr.getTargetXDegrees(); // horizontal offset
-//            double yDeg = fr.getTargetYDegrees();
 
             Pose3D pose3D = fr.getTargetPoseCameraSpace();
-            Position p = pose3D.getPosition();
+            Position p = pose3D.getPosition().toUnit(DistanceUnit.INCH);
 
             double x = p.x;
             double y = p.y;
@@ -68,16 +63,11 @@ public class LimeLightSubsystem extends SubsystemBase {
 
             distance = Math.sqrt(x*x + y*y + z*z);
 
-//            double ty = yDeg; // vertical offset in degrees
-//            distance = estimateDistance(CAMERA_HEIGHT, TARGET_HEIGHT, CAMERA_MOUNT_ANGLE, ty);
-
             telemetry.addData("Tag ID", tagID);
             telemetry.addData("x", x);
             telemetry.addData("y", y);
             telemetry.addData("z", z);
-            //telemetry.addData("Horizontal Angle", "%.2f deg", xDeg);
-            //telemetry.addData("Vertical Angle", "%.2f deg", yDeg);
-            telemetry.addData("Estimated Distance", "%.2f meters", distance);
+            telemetry.addData("Estimated Distance", "%.2f inches", distance);
             ;
         } else {
             telemetry.addData("Limelight", "No AprilTags detected");
@@ -112,57 +102,5 @@ public class LimeLightSubsystem extends SubsystemBase {
         // Optional: reduce telemetry update frequency
         telemetry.setMsTransmissionInterval(20);
 
-    }
-
-   //  // AprilTag pipeline number
-
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-////        // Initialize Limelight from hardware map
-////        limelight = hardwareMap.get(Limelight3A.class, "limelight");
-//
-//
-//
-//        // Start polling the Limelight
-//
-//
-//
-//        while (opModeIsActive()) {
-//            LLResult result = limelight.getLatestResult();
-//
-//            if (result != null && result.isValid()) {
-//                // Loop through all detected AprilTags
-//                for (LLResultTypes.FiducialResult fr : result.getFiducialResults()) {
-//                    int tagID = fr.getFiducialId();
-//                    double xDeg = fr.getTargetXDegrees(); // horizontal offset
-//                    double yDeg = fr.getTargetYDegrees(); // vertical offset
-//
-//                    // Estimate distance using vertical offset
-//                    double ty = yDeg; // vertical offset in degrees
-//                    double distance = estimateDistance(CAMERA_HEIGHT, TARGET_HEIGHT, CAMERA_MOUNT_ANGLE, ty);
-//
-//                    telemetry.addData("Tag ID", tagID);
-//                    telemetry.addData("Horizontal Angle", "%.2f deg", xDeg);
-//                    telemetry.addData("Vertical Angle", "%.2f deg", yDeg);
-//                    telemetry.addData("Estimated Distance", "%.2f meters", distance);
-//                }
-//            } else {
-//                telemetry.addData("Limelight", "No AprilTags detected");
-//            }
-//
-//            telemetry.update();
-//        }
-//
-//        limelight.stop();
-//    }
-
-    /**
-     * Estimate distance to target using camera height, target height,
-     * camera mounting angle, and vertical offset (ty)
-     */
-    private double estimateDistance(double cameraHeight, double targetHeight, double cameraAngleDeg, double verticalOffsetDeg) {
-        double a1 = Math.toRadians(cameraAngleDeg);
-        double a2 = Math.toRadians(verticalOffsetDeg);
-        return (targetHeight - cameraHeight) / Math.tan(a1 + a2);
     }
 }
