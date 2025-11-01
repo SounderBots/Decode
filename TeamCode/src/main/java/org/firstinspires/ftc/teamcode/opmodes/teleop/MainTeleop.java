1b99df47bb0f94f27b66a3beefd5b3de2dc0fa86
AaronW6489 <aaron.w8656@gmail.com>
2025-11-01 11:39:02 -0700

    limelight teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
@@ -12,6 +12,7 @@ import org.firstinspires.ftc.teamcode.command.AutoLoadShooterCommand;
 import org.firstinspires.ftc.teamcode.command.LaunchAndReload;
 import org.firstinspires.ftc.teamcode.opmodes.OpModeTemplate;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.LimeLightSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SingleShooter;
 import org.firstinspires.ftc.teamcode.subsystems.TeleopDrivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.TransferChamber;
@@ -23,6 +24,8 @@ public class MainTeleop extends OpModeTemplate {
     Intake intake;
 
     SingleShooter shooter;
+
+    LimeLightSubsystem limeLight;
 
     TransferChamber transfer;
     @Config
@@ -42,6 +45,7 @@ public class MainTeleop extends OpModeTemplate {
         this.intake = new Intake(hardwareMap, operatorGamepad, telemetry);
         this.shooter = new SingleShooter(hardwareMap, operatorGamepad, telemetry);
         this.transfer = new TransferChamber(hardwareMap, operatorGamepad, telemetry);
+        this.limeLight = new LimeLightSubsystem(hardwareMap, telemetry);
 
         operatorGamepad.getGamepadButton(GamepadKeys.Button.Y)
                 .whenPressed(new InstantCommand(shooter::ToggleShooter, shooter));
@@ -71,7 +75,7 @@ public class MainTeleop extends OpModeTemplate {
                         )
                 );
 
-        register(drive, intake, shooter);
+        register(drive, intake, shooter, limeLight);
         schedule(
                 new AutoLoadShooterCommand(telemetry, transfer),
                 new AutoIntakeCommand(telemetry, transfer)
