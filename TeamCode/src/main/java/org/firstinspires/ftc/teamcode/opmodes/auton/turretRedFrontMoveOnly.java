e5122d51eaa550607c40405743fd4efee8ea1f25
AaronW6489 <aaron.w8656@gmail.com>
2026-01-02 15:30:52 -0800

    added red back auto, and sped up red front auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/turretRedFrontMoveOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/turretRedFrontMoveOnly.java
@@ -19,20 +19,22 @@ public class turretRedFrontAutoMoveOnly extends AutonBase {
     public static double startPositionX = 128.0;
     public static double startPositionY = 112.5;
 
-    public static double DRIVE_SPEED = 0.6;
+    public static double DRIVE_SPEED = 0.8;
+    public static double returnSpeed = 1;
 
     public static double shootingSpotX = 100;
     public static double shootingSpotY = 95;
 
-    public static double intakeLine1X = 125;
-    public static double intakeLine1Y = 84;
+    public static double intakeLine1X = 120;
+    public static double intakeLine1Y = 86;
 
-    public static double intakeLine2X = 120;
+    public static double intakeLine2X = 112.5;
     public static double intakeLine2Y = 60;
 
-    public static double classifierLineX = 128;
+    public static double classifierLineX = 123;
     public static double classifierLineY = 63;
-    public static double classifierStrafe = 61;
+    public static double classifierStrafeX = 134;
+    public static double classifierStrafeY = 58;
 
     public static double endParkY = 72;
     public static double endParkX = 115;
@@ -63,8 +65,7 @@ public class turretRedFrontAutoMoveOnly extends AutonBase {
         //clear classifier
         Pose p10 = new Pose(shootingSpotX, classifierLineY, heading);
         Pose p11 = new Pose(classifierLineX, classifierLineY, heading);
-        Pose rotate45 = new Pose(classifierLineX, classifierLineY, 0.78539816);
-        Pose strafeRight2 = new Pose(classifierLineX, classifierStrafe, 0.78539816);
+        Pose intakeFromClassifier = new Pose(classifierStrafeX, classifierStrafeY, 0.78539816);
         Pose p12 = new Pose(shootingSpotX, classifierLineY, heading);
         Pose p13 = new Pose(shootingSpotX, shootingSpotY, heading);
 
@@ -84,15 +85,14 @@ public class turretRedFrontAutoMoveOnly extends AutonBase {
                 .andThen(commandFactory.moveTo(p7, PathType.LINE, driveSpeed))
                 .andThen(commandFactory.stopIntake())
                 .andThen(commandFactory.moveTo(p8, PathType.LINE, driveSpeed))
-                .andThen(commandFactory.moveTo(p13, PathType.LINE, driveSpeed))
+                .andThen(commandFactory.moveTo(p9, PathType.LINE, driveSpeed))
                 .andThen(commandFactory.sleep(lineTwoTime))
 
                 //clear and take from classifier
                 .andThen(commandFactory.moveTo(p10, PathType.LINE, driveSpeed))
                 .andThen(commandFactory.startIntake())
                 .andThen(commandFactory.moveTo(p11, PathType.LINE, driveSpeed))
-                .andThen(commandFactory.moveTo(rotate45, PathType.LINE, driveSpeed))
-                .andThen(commandFactory.moveTo(strafeRight2, PathType.LINE, driveSpeed))
+                .andThen(commandFactory.moveTo(intakeFromClassifier, PathType.LINE, driveSpeed))
                 .andThen(commandFactory.sleep(classifierIntakeTime))
                 .andThen(commandFactory.stopIntake())
                 .andThen(commandFactory.moveTo(p12, PathType.LINE, driveSpeed))
@@ -109,7 +109,7 @@ public class turretRedFrontAutoMoveOnly extends AutonBase {
                 .andThen(commandFactory.sleep(lineOneTime))
 
                 //park in front of classifier
-                .andThen(commandFactory.moveTo(p14, PathType.LINE, driveSpeed));
+                .andThen(commandFactory.moveTo(p14, PathType.LINE, returnSpeed));
     }
 
     @Override
